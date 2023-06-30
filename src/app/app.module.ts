9120b79cf42f5868f34c1c6a2892046818fa7954
Bryan España <bryanespana21550@gmail.com>
2023-06-30 15:43:55 -0600

    Carousel inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselPlanetsComponent } from './inicio/carousel-planets/carousel-planets.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,8 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    NgbModule,
+    NgbCarouselModule
     
   ],
   providers: [],
--- src/app/inicio/carousel-planets/carousel-planets.component.ts
+++ src/app/inicio/carousel-planets/carousel-planets.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel-planets',
   templateUrl: './carousel-planets.component.html',
   styleUrls: ['./carousel-planets.component.scss']
 })
-export class CarouselPlanetsComponent {
+export class CarouselPlanetsComponent{
 
+  images = [
+    {name: 'assets/tierra.png', who: 'Planet', info: 'Earth' },
+    {name: 'assets/lunaaa.png', who: 'Satelite', info: 'Moon' },
+    {name: 'assets/marte.png', who: 'Planet', info: 'Mars' },
+  ];
+ 
 }
